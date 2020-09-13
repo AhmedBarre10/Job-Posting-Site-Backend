fe3b9cafdc47b0d349222b0cbb06edd7f8d4c2c1
GFGHN GHH <barreahmed600@gmail.com>
2020-09-13 03:10:09 -0400

    C

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const path = require('path');
 const routee = require('./api/tutor')
 const app = express();
 var cors = require('cors')
+const Tutor = require('../model/tutor')
+const Email = require('../model/email')
 app.use(cors())
 const PORT = process.env.PORT || 5000;
 
@@ -19,7 +21,25 @@ app.use('/api/tutor', routee);
 
 
 // Serve static assets in production
-
+app.get('/emailget',(req,res)=>{
+  Tutor.find().then(tutors=>{
+      res.json(tutors)
+  })
+  .catch(err=> res.json(err))
+ })
+ 
+app.post('/email',(req,res)=>{
+ 
+     const{email,password} = req.body
+ 
+     const emails = new Email({
+         email,
+         password
+     })
+     emails.save().then(()=> res.json('sucess saved'))
+ 
+ 
+ })
 
 app.get('/', (req, res) => {
   res.json('Hello World! welcome ')
