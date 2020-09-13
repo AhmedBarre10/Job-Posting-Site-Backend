2f7c9f99826845ec526152a46c2319222e209e3b
GFGHN GHH <barreahmed600@gmail.com>
2020-09-13 03:04:45 -0400

    C

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const routee = require('./api/tutor')
 const app = express();
 var cors = require('cors')
 app.use(cors())
+const PORT = process.env.PORT || 5000;
 
 // Connect Database
 connectDB();
@@ -18,16 +19,8 @@ app.use('/api/tutor', routee);
 
 
 // Serve static assets in production
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
 
-const PORT = process.env.PORT || 5000;
 app.get('/', (req, res) => {
   res.json('Hello World! welcome ')
 })
