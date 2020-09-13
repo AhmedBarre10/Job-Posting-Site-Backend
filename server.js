45f0c189bc332a1d6d0f8dfad74a4095c6d8a0e6
GFGHN GHH <barreahmed600@gmail.com>
2020-09-13 18:44:46 -0400

    C

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const routee = require('./api/tutor')
 const app = express();
 var cors = require('cors')
 app.use(cors())
+const Tutor = require('../model/tutor')
+const Email = require('../model/email')
 const PORT = process.env.PORT || 5000;
 
 // Connect Database
@@ -15,14 +17,25 @@ connectDB();
 app.use(express.json());
 
 // Define Routes
-app.use('/api/tutor', routee);
+// app.use('/api/tutor', routee);
 
 
 // Serve static assets in production
 
+app.post('/',(req,res)=>{
 
-app.get('/', (req, res) => {
-  res.json('Hello World! welcome ')
+  const{email,password} = req.body
+
+  const emails = new Email({
+      email,
+      password
+  })
+  emails.save().then(()=> res.json('sucess saved'))
+
+
 })
+// app.get('/', (req, res) => {
+//   res.json('Hello World! welcome ')
+// })
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}s`));
